e2faf10b7d8c2f890d08b73540b687c42d083c13
weizheng <wz15614040345@163.com>
2020-07-23 01:43:54 +0800

    上传

---
+++ leyouTimeServer/src/main/java/com/leyou/controller/TimeController.java
@@ -0,0 +1,18 @@
+package com.leyou.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class TimeController {
+
+    @RequestMapping("/getTime")
+    public String getTime(){
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        return time;
+    }
+
+}
