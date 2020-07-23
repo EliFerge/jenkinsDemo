5a792fe961d3cecca31d3f7b141600be6c36b275
weizheng <wz15614040345@163.com>
2020-07-23 09:31:45 +0800

    上传

--- leyouTimeServer/build.sh
+++ leyouTimeServer/build.sh
@@ -1,3 +1,3 @@
 docker images
-docker build -f /root/.jenkins/workspace/webGenkinsDemo/leyouTimeServer .
+docker build -f /root/.jenkins/workspace/webGenkinsDemo/leyouTimeServer/Dockerfile -t act
 docker images
