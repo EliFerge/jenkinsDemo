7e6580eee9386d3e73888f4305b9867c79f27b94
weizheng <wz15614040345@163.com>
2020-07-23 09:21:32 +0800

    上传

--- leyouTimeServer/run.sh
+++ leyouTimeServer/run.sh
@@ -2,6 +2,6 @@ docker ps -a
 
 docker stop actdemo
 docker rm -f actdemo
-docker run -d --name actdemo -p 8000:8000 -p 5005:5005 act
+docker run -d --name actdemo -p 8000:8000 -p 5005:5005
 
 docker ps -a
